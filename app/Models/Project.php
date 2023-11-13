8c80e96abb30d0d14490aab4c9829d75d383f73f
EsterDahri <esterdahri@gmail.com>
2023-11-13 18:14:58 +0100

    add methods in Type and Project models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+
+    public function type()
+    {
+        return $this->hasMany(Type::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
